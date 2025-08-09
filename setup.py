d67bea46504f36d50e54882c4f51bc213d500dfb
sonsoleslp <poynting.slp@gmail.com>
2025-08-10 00:06:06 +0300

    New prompt eng interface

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='LAILA',
 
     py_modules=["app","config","API_Settings","wsgi"],  # Only include your main script/module
-    packages=["model","views","static/css","static/js","db"],
+    packages=["model","views","static/css","static/js","db","prompts"],
     install_requires=open("requirements.txt").read().splitlines(),
     include_package_data=True,
     package_data={
